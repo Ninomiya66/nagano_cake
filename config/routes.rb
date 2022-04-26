62f87168566f17e737cb720116843ccab59aa82f
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-04-26 09:33:46 +0000

    header作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
@@ -16,6 +16,12 @@ Rails.application.routes.draw do
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+  
+    get '/' => 'homes#top'
+  
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
