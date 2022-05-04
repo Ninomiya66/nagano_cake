f41f766d1febfdf87f5c4e02032c9006c1a65504
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-05-04 06:08:11 +0000

    order details

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
