1a0a1a0aad50404b91dd1d1c0ac14134f377852e
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-05-05 10:39:43 +0000

    order details

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,7 +23,8 @@ class Item < ApplicationRecord
   end
 
   has_many :cart_items, dependent: :destroy
-
+  has_many :order_details, dependent: :destroy
+  has_many :orders, through: :order_details
   belongs_to :genre
 
   validates :genre_id, presence: true
