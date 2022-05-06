7062151dcfca24afeb8b64d8937c3dba6a9d2194
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-05-06 02:39:49 +0000

    admin orders

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,48 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def update
+
+    @order_detail = OrderDetail.find(params[:id])
+
+    @order_detail.update(order_detail_params)
+
+    @order = @order_detail.order
+
+    # 製作ステータスを[製作中(2)],注文ステータスを[製作中(2)]
+    if @order_detail.make_status == "製作中"
+
+      @order.update(order_status: 2)
+
+      flash[:notice] = "ステータスを更新しました。"
+
+      @order.save
+
+    end
+
+    # 注文個数と製作済みの個数が同じなら
+    if @order.order_details.amount == @order.order_details.where(make_status: 3).amount
+
+      @order.update(order_status: 3)
+
+      flash[:notice] = "ステータを更新しました。"
+
+      @order.save
+
+    end
+
+    redirect_to request.referer
+
+  end
+
+  # ストロングパラメータ
+
+  def order_detail_params
+
+    prams.require(:order_detail).permit(:order_id, :make_status, :amount)
+
+  end
+
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,42 @@
 class Admin::OrdersController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def show
+
+    @order = Order.find(params[:id])
+
+    @order_details = @order.order_details
+
   end
+
+  def update
+
+    @order = Order.find(params[:id])
+
+    @order.update(order_params)
+
+    flash[:notice] = "ステータスを更新しました。"
+
+    redirect_to admin_order_path(@order)
+
+    if @order_status == "入金確認"
+
+      @order.order_details.each do |order_detail|
+
+        order_detail.update(make_status: 1)
+
+      end
+
+    end
+
+  end
+
+  # ストロングパラメータ
+  def order_params
+
+    params.require(:order).permit(:price_total, :postage, :postal_code, :address, :name, :payment_method, :order_status, :make_status)
+
+  end
+
 end
