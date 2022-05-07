ee57d3bf18d71c55a9312582228b0aaf0e8901d0
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-05-07 10:19:25 +0000

    レイアウト開始中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,11 +1,15 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
-    
+
+    @items = Item.all.order(created_at: :asc)
+
+    @genres = Genre.all
+
   end
 
   def about
-    
+
   end
-  
+
 end
